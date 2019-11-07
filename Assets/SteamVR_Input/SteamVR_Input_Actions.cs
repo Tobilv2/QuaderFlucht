dd5e089db26925c3afc889e61a3e6d5b548d6c46
Prostok <35338559+Prostok@users.noreply.github.com>
2019-11-07 12:40:43 +0100

    Added input for right touchpad

--- Assets/SteamVR_Input/SteamVR_Input_Actions.cs
+++ Assets/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -47,6 +47,8 @@ namespace Valve.VR
         
         private static SteamVR_Action_Boolean p_default_laser;
         
+        private static SteamVR_Action_Vector2 p_default_lasergrabcontrol;
+        
         private static SteamVR_Action_Vibration p_default_Haptic;
         
         private static SteamVR_Action_Vector2 p_platformer_Move;
@@ -183,6 +185,14 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Vector2 default_lasergrabcontrol
+        {
+            get
+            {
+                return SteamVR_Actions.p_default_lasergrabcontrol.GetCopy<SteamVR_Action_Vector2>();
+            }
+        }
+        
         public static SteamVR_Action_Vibration default_Haptic
         {
             get
@@ -265,6 +275,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_touchpadjump,
                     SteamVR_Actions.default_grab,
                     SteamVR_Actions.default_laser,
+                    SteamVR_Actions.default_lasergrabcontrol,
                     SteamVR_Actions.default_Haptic,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
@@ -289,6 +300,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_touchpadjump,
                     SteamVR_Actions.default_grab,
                     SteamVR_Actions.default_laser,
+                    SteamVR_Actions.default_lasergrabcontrol,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -322,6 +334,7 @@ namespace Valve.VR
                     SteamVR_Actions.buggy_Throttle};
             Valve.VR.SteamVR_Input.actionsVector2 = new Valve.VR.SteamVR_Action_Vector2[] {
                     SteamVR_Actions.default_touchpadmovement,
+                    SteamVR_Actions.default_lasergrabcontrol,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.buggy_Steering};
             Valve.VR.SteamVR_Input.actionsVector3 = new Valve.VR.SteamVR_Action_Vector3[0];
@@ -341,6 +354,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_touchpadjump,
                     SteamVR_Actions.default_grab,
                     SteamVR_Actions.default_laser,
+                    SteamVR_Actions.default_lasergrabcontrol,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -366,6 +380,7 @@ namespace Valve.VR
             SteamVR_Actions.p_default_touchpadjump = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/touchpadjump")));
             SteamVR_Actions.p_default_grab = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/grab")));
             SteamVR_Actions.p_default_laser = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/laser")));
+            SteamVR_Actions.p_default_lasergrabcontrol = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/default/in/lasergrabcontrol")));
             SteamVR_Actions.p_default_Haptic = ((SteamVR_Action_Vibration)(SteamVR_Action.Create<SteamVR_Action_Vibration>("/actions/default/out/Haptic")));
             SteamVR_Actions.p_platformer_Move = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/platformer/in/Move")));
             SteamVR_Actions.p_platformer_Jump = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/platformer/in/Jump")));
