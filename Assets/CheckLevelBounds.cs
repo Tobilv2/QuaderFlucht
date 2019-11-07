2606d45afd52fd9748591d96bb76585f187b1250
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2019-11-07 14:25:13 +0100

    added black hole

--- Assets/CheckLevelBounds.cs
+++ Assets/CheckLevelBounds.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Photon.Pun;
+﻿using Photon.Pun;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class CheckLevelBounds : MonoBehaviourPunCallbacks
 {
     private void OnTriggerEnter(Collider other)
     {
-        PhotonNetwork.LoadLevel("Level2");
+        PhotonNetwork.LoadLevel("Level3");
     }
 }
 
