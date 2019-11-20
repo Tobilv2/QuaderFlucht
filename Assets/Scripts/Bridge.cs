4b6b5d2703981d921d42fdd1b8c9f5eb5b9a72fe
Yannik Gottwald <gottwald@stud.hs-heilbronn.de>
2019-11-20 15:00:36 +0100

    Created Base of Level 6

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    private new Animation animation;
+    private bool BridgeIsOpen;
+
+    private void Start()
+    {
+
+        BridgeIsOpen = true;
+        
+    }
+
+    public void OpenDoor()
+    {
+        if (!BridgeIsOpen)
+        {
+            GetComponent<Animation>().Play("BridgeAnimation");
+            BridgeIsOpen = true;
+        }
+    } 
+    
+    public void CloseDoor()
+    {
+        if (BridgeIsOpen)
+        {
+//            GetComponent<Animation>().Play("CloseDoor");
+            BridgeIsOpen = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ButtonHandlerBridge.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ButtonHandlerBridge : MonoBehaviour
+{
+
+    public Bridge bridge;
+        
+    private void OnTriggerExit(Collider other)
+    {
+            bridge.CloseDoor();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bridge.OpenDoor();
+    }
+}
+
