544c96009ea63053244c34a3476c4c9d4df05c15
MaxStricker <[stricker@stud.hs-heilbronn.de]>
2019-11-20 12:27:24 +0100

    added level change
    added

--- Assets/LoadNextLevel.cs
+++ Assets/LoadNextLevel.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Photon.Pun;
 
 public class LoadNextLevel : MonoBehaviourPunCallbacks
 {
+    private void Start()
+    {
+        PlayerPrefs.SetInt("level",2);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        PhotonNetwork.LoadLevel("Level4");
+        PlayerPrefs.SetInt("level",PlayerPrefs.GetInt("level")+1);
+        PhotonNetwork.LoadLevel(PlayerPrefs.GetInt("level"));
+    }
+
+    private void OnApplicationQuit()
+    {
+        PlayerPrefs.DeleteAll();
     }
 }
---
+++ Assets/ReloadLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+  private void OnTriggerEnter(Collider other)
+  {
+    PhotonNetwork.LoadLevel(PlayerPrefs.GetInt("level"));
+  }
+}
