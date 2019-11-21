dcdae10f306521fea76ca53d3f4ed32d3d111f5d
Prostok <35338559+Prostok@users.noreply.github.com>
2019-11-21 12:05:10 +0100

    Fixed mulitple players on respawn

--- Assets/CheckLevelBounds.cs
+++ Assets/CheckLevelBounds.cs
@@ -7,6 +7,16 @@ public class CheckLevelBounds : MonoBehaviourPunCallbacks
 {
     private void OnTriggerEnter(Collider other)
     {
+        if (PhotonNetwork.NickName == "VR")
+        {
+            PhotonNetwork.DestroyAll();
+            photonView.RPC("ReloadScenesForAllPlayers",RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    void ReloadScenesForAllPlayers()
+    {
         PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().name);
     }
     
