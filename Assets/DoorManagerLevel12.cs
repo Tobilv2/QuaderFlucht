fb6c073c664f6d95a4eee5776e3c2d711e14f278
Prostok <35338559+Prostok@users.noreply.github.com>
2020-01-15 12:04:46 +0100

    aifjkajfksfjas

---
+++ Assets/DoorManagerLevel12.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManagerLevel12 : MonoBehaviour
+{
+    public enum DoorColors
+    {
+       Green,
+       Red,
+       Yellow
+       
+    }
+    public List<GameObject> greenDoors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            ChangeState(DoorColors.Green);
+        }
+    }
+
+    public void ChangeState(DoorColors col)
+    {
+        if (col == DoorColors.Green)
+        {
+            foreach (GameObject greenDoor in greenDoors)
+            {
+                greenDoor.GetComponent<DoorScriptLevel12>().ChangeState();
+            }
+        }
+    }
+}
