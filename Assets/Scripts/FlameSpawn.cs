968d09361c58d5183e1247b913aa4b7a6e27a9d9
Prostok <35338559+Prostok@users.noreply.github.com>
2020-01-16 13:03:44 +0100

    Deleted trash

---
+++ Assets/Scripts/FlameSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class FlameSpawn : MonoBehaviourPun
+{
+    public GameObject flamePrefab;
+
+    private void Update()
+    {
+        if (PhotonNetwork.NickName == "mobile")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Camera cam = GameObject.FindWithTag("MobilePlayer").GetComponentInChildren<Camera>();
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    photonView.RPC("InstantiateWithPhoton", RpcTarget.All, hit.point);
+                }
+            }
+        }
+    }
+
+    [PunRPC]
+    void InstantiateWithPhoton(Vector3 pos)
+    {
+        GameObject gO = Instantiate(flamePrefab, pos,
+            Quaternion.identity);
+        Destroy(gO, 0.5f);
+    }
+}
---
+++ Assets/Scripts/ReloadLevel.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+
+public class ReloadLevel : MonoBehaviourPunCallbacks
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.CompareTag("VRPlayer");
+        PhotonNetwork.LoadLevel("Level3");
+    }
+}
+
