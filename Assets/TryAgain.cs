69a9050cafd0c19e95c41ec06d5c2f4125e978ae
Mendim Klimenta <klimenta@stud.hs-heilbronn.de>
2020-01-16 11:33:08 +0100

    added tryAgain

---
+++ Assets/TryAgain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TryAgain : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+    private void Start()
+    {
+        audioSource.clip = audioClip;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (PhotonNetwork.NickName == "VR" && other.CompareTag("VRPlayer"))
+        {
+            audioSource.Play();
+        }
+    }
+}
